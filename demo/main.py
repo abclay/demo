da476a2ad74ee7c427fea2c8d2e6cca05e9aad56
Alex Clay <aclay@tetrascience.com>
2022-01-14 15:50:43 -0500

    working commit

--- demo/main.py
+++ demo/main.py
@@ -1,25 +1,20 @@
 """
 This is a normal file
 """
-import sys   # unused import LOL
-bad_formatting = [
-1,
-2,
-{3:
-4}]
+
+bad_formatting = [1, 2, {3: 4}]
 
 
 def fib(num):
     """
     Return that fib num
     """
-    # This line fails the test now
-    if num < 5:     
+    if num <= 1:
         print(f"{num} == 1")
         return 1
     prev = 1
     cur = 1
-    for nth in range(0, num - 1):
+    for _ in range(0, num - 1):
         prev, cur = cur, prev + cur
     print(f"{num} == {cur}")
     return cur
@@ -29,13 +24,8 @@ def main(num):
     """
     Prints the first N fibonacci numbers
     """
-    prev = 1
-    cur = 1
-    print(f"1: {prev}")
-    print(f"2: {cur}")
-    for nth in range(3, num):
-        prev, cur = cur, prev + cur
-        print(f"{nth}: {cur}")
+    for nth in range(num):
+        print(f"{nth}: {fib(nth)}")
 
 
 if __name__ == "__main__":
